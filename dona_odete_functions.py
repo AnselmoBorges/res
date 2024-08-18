d2488974e4129a908198f4020bf7bcef82911e55
hugoadventist <28987448+hugoadventist@users.noreply.github.com>
2024-08-18 07:43:07 -0300

    chore: mudando a configuracao do ambiente virtual para deploy na streamlit

--- dona_odete_functions.py
+++ dona_odete_functions.py
@@ -7,7 +7,7 @@ def gerar_audio_resposta(resposta):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
         tts.tts_to_file(
             text=resposta,
-            speaker_wav="As-melhores-historias-hilariantes-da-minha-carreira-como-radiatriz-para-o-teatro-_U4pQ536JZNY_.wav",
+            speaker_wav="U4pQ536JZNY_.wav",
             language="pt",
             file_path=temp_file.name,
         )
